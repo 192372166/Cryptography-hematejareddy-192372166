369f749d0ceeab18ac40ea2fe7b77719035013ae
192372166 <160738246+192372166@users.noreply.github.com>
2026-02-12 10:24:55 +0530

    Create 9.c program for pt-109.c

---
+++ 9.c program for pt-109.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+void playfair_encrypt(char *message, char *key) {
+    // Implementation of Playfair encryption algorithm
+}
+
+int main() {
+    char key[] = "PT109";
+    char message[] = "KXJEY UREBE ZWEHE WRYTU HEYFS KREHE GOYFI WTTTU OLKSY CAJPO BOTEI ZONTX BYBNT GONEY CUZWR GDSON SXBOU YWRHE BAAHY USEDQ";
+
+    playfair_encrypt(message, key);
+
+    return 0;
+}
